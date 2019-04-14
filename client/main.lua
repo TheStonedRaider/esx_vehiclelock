a92fca0a619972576da76cd5c90b405a7cba315c
TheStonedRaider <joebopie@googlemail.com>
2019-04-14 02:38:04 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,105 +1,116 @@
 ESX               = nil
-
-local Keys = {
-	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}
-
-local isRunningWorkaround = false
+local playerCars = {}
 
 Citizen.CreateThread(function()
-	while ESX == nil do
+
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(0)
-	end
 end)
+--Citizen.CreateThread(function()
+--    while ESX == nil do
+--        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+--       Citizen.Wait(2000)
+--    end
+--
+--    while ESX.GetPlayerData().job == nil do
+--       Citizen.Wait(100)
+--    end
+--
+--    PlayerLoaded = true
+--    PlayerData = ESX.GetPlayerData()
+--
+--end)
+-- 
+function OpenCloseVehicle()
 
-function StartWorkaroundTask()
-	if isRunningWorkaround then
-		return
-	end
+        local _source = source
+        local PlayerData = ESX.GetPlayerData(_source)
 
-	local timer = 0
-	local playerPed = PlayerPedId()
-	isRunningWorkaround = true
-
-	while timer < 100 do
-		Citizen.Wait(0)
-		timer = timer + 1
-
-		local vehicle = GetVehiclePedIsTryingToEnter(playerPed)
-
-		if DoesEntityExist(vehicle) then
-			local lockStatus = GetVehicleDoorLockStatus(vehicle)
-
-			if lockStatus == 4 then
-				ClearPedTasks(playerPed)
-			end
-		end
-	end
-
-	isRunningWorkaround = false
+	local playerPed = GetPlayerPed(-1)
+	local coords    = GetEntityCoords(playerPed)
+                local pos = GetEntityCoords(PlayerPedId(), true)
+                local targetPos = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 1.0, 6.0, -4.0)
+                local rayCast = StartShapeTestCapsule(pos.x, pos.y, pos.z, targetPos.x, targetPos.y, targetPos.z, 2, 10, PlayerPedId(), 7)
+                local _,hit,_,_,vehicle = GetShapeTestResult(rayCast)
+                if hit and DoesEntityExist(vehicle) and IsEntityAVehicle(vehicle) then
+                    currentSelection = vehicle
 end
 
-function ToggleVehicleLock()
-	local playerPed = PlayerPedId()
-	local coords = GetEntityCoords(playerPed)
-	local vehicle
-
-	Citizen.CreateThread(function()
-		StartWorkaroundTask()
-	end)
-
-	if IsPedInAnyVehicle(playerPed, false) then
-		vehicle = GetVehiclePedIsIn(playerPed, false)
-	else
-		vehicle = GetClosestVehicle(coords, 8.0, 0, 70)
-	end
-
-	if not DoesEntityExist(vehicle) then
-		return
-	end
-
-	ESX.TriggerServerCallback('esx_vehiclelock:requestPlayerCars', function(isOwnedVehicle)
-
-		if isOwnedVehicle then
-			local lockStatus = GetVehicleDoorLockStatus(vehicle)
-
-			if lockStatus == 1 then -- unlocked
-				SetVehicleDoorsLocked(vehicle, 4)
+	ESX.TriggerServerCallback('esx_vehiclelock:requestPlayerCars', function(isOwnedVehicle, job)
+		if isOwnedVehicle or PlayerData.job ~= nil and PlayerData.job.name == 'police' then
+			local locked = GetVehicleDoorLockStatus(vehicle)
+			if locked == 1 then -- if unlocked
+				SetVehicleDoorsLocked(vehicle, 2)
 				PlayVehicleDoorCloseSound(vehicle, 1)
-
-				TriggerEvent('chat:addMessage', { args = { _U('message_title'), _U('message_locked') } })
-			elseif lockStatus == 4 then -- locked
+				ESX.ShowNotification("You have ~r~Locked~s~ the Vehicle.")
+                  sendNotification('You have Locked the Vehicle', 'error', 2000)
+			elseif locked == 2 then -- if locked
 				SetVehicleDoorsLocked(vehicle, 1)
 				PlayVehicleDoorOpenSound(vehicle, 0)
-
-				TriggerEvent('chat:addMessage', { args = { _U('message_title'), _U('message_unlocked') } })
+				ESX.ShowNotification("You Have ~g~opened~s~ the Vehicle.")
+             	  sendNotification('You Have opened the Vehicle', 'success', 2000)
 			end
-		end
 
-	end, ESX.Math.Trim(GetVehicleNumberPlateText(vehicle)))
+		else
+
+			ESX.ShowNotification("~r~You do not own this vehicle.")
+		end
+	end, GetVehicleNumberPlateText(vehicle))
+end
+
+
+function OpenCloseVehicle2()
+        local _source = source
+        local PlayerData = ESX.GetPlayerData(_source)
+
+	local playerPed = GetPlayerPed(-1)
+	local coords    = GetEntityCoords(playerPed)
+	local vehicle = GetVehiclePedIsIn(playerPed, false)
+                if DoesEntityExist(vehicle) and IsEntityAVehicle(vehicle) then
+                    currentSelection = vehicle
+end
+
+	ESX.TriggerServerCallback('esx_vehiclelock:requestPlayerCars', function(isOwnedVehicle, job)
+		if isOwnedVehicle or PlayerData.job ~= nil and PlayerData.job.name == 'police' then
+
+			local locked = GetVehicleDoorLockStatus(vehicle)
+			if locked == 1 then -- if unlocked
+				SetVehicleDoorsLocked(vehicle, 2)
+				PlayVehicleDoorCloseSound(vehicle, 1)
+				ESX.ShowNotification("You have ~r~Locked~s~ the Vehicle.")
+              	sendNotification('You have Locked the Vehicle', 'error', 2000)
+			elseif locked == 2 then -- if locked
+				SetVehicleDoorsLocked(vehicle, 1)
+				PlayVehicleDoorOpenSound(vehicle, 0)
+				ESX.ShowNotification("You Have ~g~opened~s~ the Vehicle.")
+           	sendNotification('You Have opened the Vehicle', 'success', 2000)
+			end
+
+		else
+
+			ESX.ShowNotification("~r~You do not own this vehicle.")
+		end
+	end, GetVehicleNumberPlateText(vehicle))
 end
 
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(10)
-
-		if IsControlJustReleased(0, Keys['U']) and IsInputDisabled(0) then
-			ToggleVehicleLock()
-			Citizen.Wait(300)
-	
-		-- D-pad down on controllers works, too!
-		elseif IsControlJustReleased(0, 173) and not IsInputDisabled(0) then
-			ToggleVehicleLock()
-			Citizen.Wait(300)
+		Wait(10)
+		if IsControlJustReleased(1, 175) then
+			OpenCloseVehicle()
+			OpenCloseVehicle2()
 		end
 	end
 end)
+
+--notification
+function sendNotification(message, messageType, messageTimeout)
+	TriggerEvent("pNotify:SendNotification", {
+		text = message,
+		type = messageType,
+		queue = "lock",
+		theme = "gta",
+		timeout = messageTimeout,
+		layout = "bottomCenter"
+	})
+end
